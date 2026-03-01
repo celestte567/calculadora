e7d9da71ca5ffe1e15d5286e94b748f0f27e2d4c
celeste <celestes657rtb@gmail.com>
2026-03-01 16:14:06 -0600

    agregar validacion de numeros

--- src/calculadora.java
+++ src/calculadora.java
@@ -5,15 +5,33 @@ public class calculadora {
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
 
-        int numero1;
-        int numero2;
+        int numero1 = 0;
+        int numero2 = 0;
         int resultado;
+        boolean NumeroValido = false;
 
-        System.out.print(" ingrese el primer numero ");
-        numero1 = sc.nextInt();
 
-        System.out.print("ingresa el segundo numero ");
-        numero2 = sc.nextInt();
+        do {
+            System.out.print(" ingrese el primer numero ");
+            try {
+                numero1 = Integer.parseInt(sc.nextLine());
+                NumeroValido = true;
+            } catch (NumberFormatException e) {
+                System.out.println("ingreso un numeros incorrecto, vuelva a ingresarlo");
+            }
+        } while (!NumeroValido);
+
+        NumeroValido = false;
+
+        do {
+            System.out.print(" ingrese el segundo numero ");
+            try {
+                numero2 = Integer.parseInt(sc.nextLine());
+                NumeroValido = true;
+            } catch (NumberFormatException e) {
+                System.out.println("ingreso un numeros incorrecto, vuelva a ingresarlo");
+            }
+        } while (!NumeroValido);
 
         resultado = numero1 + numero2;
 
