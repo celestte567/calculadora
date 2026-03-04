4f573aae7087b6fddbd2bd51f3b383563be5cdc1
celeste <celestes657rtb@gmail.com>
2026-03-03 22:18:40 -0600

    dosecat, agregar funcion a la suma

--- src/calculadora.java
+++ src/calculadora.java
@@ -2,41 +2,39 @@ import java.util.Scanner;
 
 public class calculadora {
 
-    public static void main(String [] args) {
+
+    public static int sumar(int a , int b){
+        return a + b;
+    }
+
+    public static int leerNumero(Scanner sc, String mensaje){
+        int numero = 0;
+        boolean numeroValido = false;
+
+        do {
+            System.out.print(mensaje);
+            try {
+                numero = Integer.parseInt(sc.nextLine());
+                numeroValido = true;
+            } catch (NumberFormatException e) {
+                System.out.println("ingreso un numero incorrecto, vuelva a ingresar el numero correcto");
+            }
+        } while (!numeroValido);
+        return numero;
+    }
+
+    public static void main(String[] args)
+    {
+
         Scanner sc = new Scanner(System.in);
+       int numero1 = leerNumero(sc, "ingresar el primer numero ");
+       int numero2 = leerNumero(sc, "ingresar el segundo numero");
 
-        int numero1 = 0;
-        int numero2 = 0;
-        int resultado;
-        boolean NumeroValido = false;
+       int resultado = sumar(numero1, numero2);
 
+       System.out.println("la suma es" + resultado);
 
-        do {
-            System.out.print(" ingrese el primer numero ");
-            try {
-                numero1 = Integer.parseInt(sc.nextLine());
-                NumeroValido = true;
-            } catch (NumberFormatException e) {
-                System.out.println("ingreso un numeros incorrecto, vuelva a ingresarlo");
-            }
-        } while (!NumeroValido);
-
-        NumeroValido = false;
-
-        do {
-            System.out.print(" ingrese el segundo numero ");
-            try {
-                numero2 = Integer.parseInt(sc.nextLine());
-                NumeroValido = true;
-            } catch (NumberFormatException e) {
-                System.out.println("ingreso un numeros incorrecto, vuelva a ingresarlo");
-            }
-        } while (!NumeroValido);
-
-        resultado = numero1 + numero2;
-
-        System.out.println("la suma es:" + resultado);
-
-        sc.close();
+       sc.close();
     }
 }
+
