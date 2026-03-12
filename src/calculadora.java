50200f40188f32966c4218b40e7d8ea017a0c5c6
celeste <celestes657rtb@gmail.com>
2026-03-11 22:24:51 -0600

    se agrego la opcion de menus

--- src/calculadora.java
+++ src/calculadora.java
@@ -7,6 +7,23 @@ public class calculadora {
         return a + b;
     }
 
+    public static int restar(int a, int b) {return  a - b;}
+
+    public static int multiplicar(int a, int b) {return  a * b;}
+
+    public static double dividir(int a, int b) {return (double) a/b;}
+
+    public static int mostrarMenu(Scanner sc){
+        System.out.println("----- calculadora----");
+        System.out.println("1. sumar");
+        System.out.println("2. restar");
+        System.out.println("3. multiplicar");
+        System.out.println("4. dividir");
+        System.out.println("seleccionar una opcion");
+
+        return Integer.parseInt(sc.nextLine());
+    }
+
     public static int leerNumero(Scanner sc, String mensaje){
         int numero = 0;
         boolean numeroValido = false;
@@ -23,18 +40,38 @@ public class calculadora {
         return numero;
     }
 
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       int numero1 = leerNumero(sc, "ingresar el primer numero ");
-       int numero2 = leerNumero(sc, "ingresar el segundo numero");
 
-       int resultado = sumar(numero1, numero2);
+        int opcion = mostrarMenu(sc);
 
-       System.out.println("la suma es" + resultado);
+        int numero1 = leerNumero(sc, "ingresar el primer numero");
+        int numero2 = leerNumero(sc, "ingresar el segundo numero");
 
-       sc.close();
+        switch (opcion){
+            case 1:
+                System.out.println(" la suma es:" + sumar(numero1, numero2));
+                break;
+            case 2:
+                System.out.println("la resta es:" + restar(numero1, numero2));
+                break;
+            case 3:
+                System.out.println("La multiplicacion es:" + multiplicar(numero1, numero2));
+                break;
+            case 4:
+                if(numero2 != 0){
+                    System.out.println("la diversion es" + dividir(numero1, numero2));
+                }else{
+                    System.out.println("no se puede dividir entra 0");
+                }
+                break;
+
+            default:
+                System.out.println("Opcion invilida");
+        }
+        sc.close();
     }
+
+
 }
 
